aea8b4422826df913b79ee9630e32e96040cde01
kendell <92357397+Kendellb@users.noreply.github.com>
2025-01-20 04:57:51 -0500

    inital vm setup

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    catppuccin.url = "github:catppuccin/nix";
+
+     home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+  };
+
+  outputs = { self, nixpkgs, catppuccin, ... }@inputs: {
+    nixosConfigurations = {
+      vm  = nixpkgs.lib.nixosSystem { 
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/vm/configuration.nix
+          inputs.home-manager.nixosModules.default
+	  catppuccin.nixosModules.catppuccin
+	  
+   	  {
+	  home-manager.users.kendell = {
+		imports = [
+		  ./hosts/vm/home.nix
+		  catppuccin.homeManagerModules.catppuccin
+		];
+	     };
+	  }
+        ];
+      };
+#     laptop = nixpkgs.lib.nixosSystem {
+#        specialArgs = {inherit inputs;};
+#        modules = [
+#          ./hosts/laptop/configuration.nix
+#          # inputs.home-manager.nixosModules.default
+#        ];
+#      };
+    };      
+  };
+}
