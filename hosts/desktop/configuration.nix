12ef36fccdd55c09a396b9e068a15f5b895cf4fb
Kendell <92357397+Kendellb@users.noreply.github.com>
2025-02-02 17:42:45 -0500

    20 current  2025-02-02 17:42:37  25.05.20250112.ed4a395     6.6.71                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -200,4 +200,11 @@
   #  };
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
