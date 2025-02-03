7463a1674a661b176382ea7ceecf6172eba10035
Kendell <92357397+Kendellb@users.noreply.github.com>
2025-02-03 16:31:31 -0500

    27 current  2025-02-03 16:31:23  25.05.20250112.ed4a395     6.6.71                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -220,4 +220,11 @@
     enable = true;
     extraPortals = [pkgs.xdg-desktop-portal-gtk];
   };
+  systemd.services.flatpak-repo = {
+    wantedBy = ["multi-user.target"];
+    path = [pkgs.flatpak];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 }
